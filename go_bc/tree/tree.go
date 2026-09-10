db6873a5d12a1d5bab38f7b0c79543470211dbd6
Sebb57 <sebdoussang@gmail.com>
2026-09-10 11:53:11 +0200

    feat: multiple operations with priorities

--- go_bc/tree/tree.go
+++ go_bc/tree/tree.go
@@ -1,14 +1,32 @@
 package tree
 
 import (
-	"go_bc/utils"
 	"go_bc/lexer"
+	"go_bc/utils"
 )
 
 type BinaryTreeNode struct {
 	Token lexer.Token
 	Left  *BinaryTreeNode
 	Right *BinaryTreeNode
+}
+
+func buildExpressionTree(tokens []lexer.Token) *BinaryTreeNode {
+	stack := make([]*BinaryTreeNode, 0)
+
+	for _, token := range tokens {
+		node := &BinaryTreeNode{Token: token}
+		if token.Type == lexer.Number {
+			stack = append(stack, node)
+			continue
+		}
+		node.Right = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		node.Left = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		stack = append(stack, node)
+	}
+	return stack[0]
 }
 
 func evaluate(root *BinaryTreeNode) string {
@@ -32,16 +50,61 @@ func evaluate(root *BinaryTreeNode) string {
 	return "0"
 }
 
-func createNode(token lexer.Token) *BinaryTreeNode {
-	return &BinaryTreeNode{
-		Token: token,
+func getPrecedence(token lexer.Token) int {
+	switch token.Type {
+		case lexer.Divide:
+			return 2
+		case lexer.Multiply:
+			return 2
+		default:
+			return 1
 	}
 }
 
 func Solve(input []lexer.Token) string {
-	root := createNode(input[1])
-	root.Left = createNode(input[0])
-	root.Right = createNode(input[2])
+	if len(input) == 0 {
+		return ""
+	}
 
+	operators := make([]lexer.Token, 0)
+	output := make([]lexer.Token, 0)
+
+	for _, token := range input {
+		switch token.Type {
+		case lexer.Number:
+			output = append(output, token)
+
+		case lexer.LeftParen:
+			operators = append(operators, token)
+
+		case lexer.RightParen:
+			for len(operators) > 0 {
+				top := operators[len(operators)-1]
+				operators = operators[:len(operators)-1]
+				if top.Type == lexer.LeftParen {
+					break
+				}
+				output = append(output, top)
+			}
+
+		default:
+			for len(operators) > 0 {
+				top := operators[len(operators)-1]
+				if getPrecedence(top) < getPrecedence(token) {
+					break
+				}
+				output = append(output, top)
+				operators = operators[:len(operators)-1]
+			}
+			operators = append(operators, token)
+		}
+	}
+	for len(operators) > 0 {
+		top := operators[len(operators)-1]
+		operators = operators[:len(operators)-1]
+		output = append(output, top)
+	}
+
+	var root *BinaryTreeNode = buildExpressionTree(output)
 	return evaluate(root)
 }
